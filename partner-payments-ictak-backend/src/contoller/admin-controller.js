d1a9720119995ddefdfca060ca67b1c86c36f193
Muhammad Ismail <connect.ismail@protonmail.com>
2022-08-28 00:03:04 +0530

    minor modifications - repush

--- partner-payments-ictak-backend/src/contoller/admin-controller.js
+++ partner-payments-ictak-backend/src/contoller/admin-controller.js
@@ -1,30 +1,3 @@
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-
-pdfGeneration = (requestData)=> {
-  console.log('pdfgen fn', requestData.trainingDetails.partner);
-
-  let pdfDoc = new PDFDocument;
-  pdfDoc.pipe(fs.createWriteStream(`./src/assets/work-orders/${requestData._id}.pdf`))
-    .on('err', (err)=> {
-      console.log(err);
-    });
-
-    pdfDoc.text("My Sample PDF Document");
-    pdfDoc.end();
-  // pdfDoc.pipe(fs.createWriteStream(`../assets/work-orders/${requestData.trainingDetails.partner}.pdf`));
-    // .on('err', (err)=> {
-    //   console.log(err);
-    // });
-
-  // .then((succ)=> {
-    //   console.log('succ', succ);
-    // }).catch((err)=> {
-    //   console.log('err', err);
-    // });
-
-}
-
 userListGen = (users)=> { // function to return the list of received users omitting some fields
   var userList = [];
 
